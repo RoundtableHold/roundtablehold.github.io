f7c18106f0ccb91948be4982fb8aacae85f3f321
Olivia <livwebspam@gmail.com>
2022-03-30 16:57:52 -0600

    Update sorceries.yaml + Reorder Items pages
    Redid the numbering for all the new list items to include renumbering the first spell in each broken up batch. All the untouched spells have the same numbers and everything that's been touched has a chronological number that wasn't in its original list.
    I also included the alphabetization of the Items pages. It includes a commented out line for the page I'm working on now.

--- js/search.js
+++ js/search.js
@@ -23,32 +23,8 @@
     searchTag: "#evergaols_search",
     contentTag: "#evergaols_list tbody"
   }), new Jets({
-    searchTag: "#flasks_search",
-    contentTag: "#flasks_list ul"
-  }), new Jets({
-    searchTag: "#crystal_tears_search",
-    contentTag: "#crystal_tears_list tbody"
-  }), new Jets({
-    searchTag: "#legendaries_search",
-    contentTag: "#legendaries_list ul"
-  }), new Jets({
-    searchTag: "#weapons_search",
-    contentTag: "#weapons_list ul"
-  }), new Jets({
-    searchTag: "#talismans_search",
-    contentTag: "#talismans_list tbody"
-  }), new Jets({
-    searchTag: "#sorceries_search",
-    contentTag: "#sorceries_list ul"
-  }), new Jets({
-    searchTag: "#dragon_hearts_death_roots_search",
-    contentTag: "#dragon_hearts_death_roots_list ul"
-  }), new Jets({
-    searchTag: "#memory_stones_talisman_pouches_search",
-    contentTag: "#memory_stones_talisman_pouches_list ul"
-  }), new Jets({
-    searchTag: "#whetstones_search",
-    contentTag: "#whetstones_list tbody"
+    searchTag: "#ashesofwar_search",
+    contentTag: "#ashesofwar_list tbody"
   }), new Jets({
     searchTag: "#bell_bearings_search",
     contentTag: "#bell_bearings_list ul"
@@ -56,17 +32,41 @@
     searchTag: "#cookbooks_search",
     contentTag: "#cookbooks_list ul"
   }), new Jets({
-    searchTag: "#ashesofwar_search",
-    contentTag: "#ashesofwar_list tbody"
+    searchTag: "#dragon_hearts_death_roots_search",
+    contentTag: "#dragon_hearts_death_roots_list ul"
+  }), new Jets({
+    searchTag: "#crystal_tears_search",
+    contentTag: "#crystal_tears_list tbody"
   }), new Jets({
     searchTag: "#gestures_search",
     contentTag: "#gestures_list ul"
   }), new Jets({
-    searchTag: "#paintings_search",
-    contentTag: "#paintings_list tbody"
+    searchTag: "#flasks_search",
+    contentTag: "#flasks_list ul"
   }), new Jets({
     searchTag: "#great_runes_search",
     contentTag: "#great_runes_list tbody"
+  }), new Jets({
+    searchTag: "#legendaries_search",
+    contentTag: "#legendaries_list ul"
+  }), new Jets({
+    searchTag: "#memory_stones_talisman_pouches_search",
+    contentTag: "#memory_stones_talisman_pouches_list ul"
+  }), new Jets({
+    searchTag: "#paintings_search",
+    contentTag: "#paintings_list tbody"
+  }), new Jets({
+    searchTag: "#sorceries_search",
+    contentTag: "#sorceries_list ul"
+  }), new Jets({
+    searchTag: "#talismans_search",
+    contentTag: "#talismans_list tbody"
+  }), new Jets({
+    searchTag: "#weapons_search",
+    contentTag: "#weapons_list ul"
+  }), new Jets({
+    searchTag: "#whetstones_search",
+    contentTag: "#whetstones_list tbody"
 })];
   $("#playthrough_search").keyup(function() {
     $("#playthrough_list").unhighlight();
@@ -96,41 +96,9 @@
     $("#evergaols_list").unhighlight();
     $("#evergaols_list").highlight($(this).val());
   });
-  $("#flasks_search").keyup(function() {
-    $("#flasks_list").unhighlight();
-    $("#flasks_list").highlight($(this).val());
-  });
-  $("#crystal_tears_search").keyup(function() {
-    $("#crystal_tears_list").unhighlight();
-    $("#crystal_tears_list").highlight($(this).val());
-  });
-  $("#legendaries_search").keyup(function() {
-    $("#legendaries_list").unhighlight();
-    $("#legendaries_list").highlight($(this).val());
-  });
-  $("#weapons_search").keyup(function() {
-    $("#weapons_list").unhighlight();
-    $("#weapons_list").highlight($(this).val());
-  });
-  $("#talismans_search").keyup(function() {
-    $("#talismans_list").unhighlight();
-    $("#talismans_list").highlight($(this).val());
-  });
-  $("#sorceries_search").keyup(function() {
-    $("#sorceries_list").unhighlight();
-    $("#sorceries_list").highlight($(this).val());
-  });
-  $("#dragon_hearts_death_roots_search").keyup(function() {
-    $("#dragon_hearts_death_roots_list").unhighlight();
-    $("#dragon_hearts_death_roots_list").highlight($(this).val());
-  });
-  $("#memory_stones_talisman_pouches_search").keyup(function() {
-    $("#memory_stones_talisman_pouches_list").unhighlight();
-    $("#memory_stones_talisman_pouches_list").highlight($(this).val());
-  });
-  $("#whetstones_search").keyup(function() {
-    $("#whetstones_list").unhighlight();
-    $("#whetstones_list").highlight($(this).val());
+  $("#ashesofwar_search").keyup(function() {
+    $("#ashesofwar_list").unhighlight();
+    $("#ashesofwar_list").highlight($(this).val());
   });
   $("#bell_bearings_search").keyup(function() {
     $("#bell_bearings_list").unhighlight();
@@ -140,21 +108,53 @@
     $("#cookbooks_list").unhighlight();
     $("#cookbooks_list").highlight($(this).val());
   });
-  $("#ashesofwar_search").keyup(function() {
-    $("#ashesofwar_list").unhighlight();
-    $("#ashesofwar_list").highlight($(this).val());
+  $("#dragon_hearts_death_roots_search").keyup(function() {
+    $("#dragon_hearts_death_roots_list").unhighlight();
+    $("#dragon_hearts_death_roots_list").highlight($(this).val());
+  });
+  $("#crystal_tears_search").keyup(function() {
+    $("#crystal_tears_list").unhighlight();
+    $("#crystal_tears_list").highlight($(this).val());
   });
   $("#gestures_search").keyup(function() {
     $("#gestures_list").unhighlight();
     $("#gestures_list").highlight($(this).val());
   });
-  $("#paintings_search").keyup(function() {
-    $("#paintings_list").unhighlight();
-    $("#paintings_list").highlight($(this).val());
+  $("#flasks_search").keyup(function() {
+    $("#flasks_list").unhighlight();
+    $("#flasks_list").highlight($(this).val());
   });
   $("#great_runes_search").keyup(function() {
     $("#great_runes_list").unhighlight();
     $("#great_runes_list").highlight($(this).val());
+  });
+  $("#legendaries_search").keyup(function() {
+    $("#legendaries_list").unhighlight();
+    $("#legendaries_list").highlight($(this).val());
+  });
+  $("#memory_stones_talisman_pouches_search").keyup(function() {
+    $("#memory_stones_talisman_pouches_list").unhighlight();
+    $("#memory_stones_talisman_pouches_list").highlight($(this).val());
+  });
+  $("#paintings_search").keyup(function() {
+    $("#paintings_list").unhighlight();
+    $("#paintings_list").highlight($(this).val());
+  });
+  $("#sorceries_search").keyup(function() {
+    $("#sorceries_list").unhighlight();
+    $("#sorceries_list").highlight($(this).val());
+  });
+  $("#talismans_search").keyup(function() {
+    $("#talismans_list").unhighlight();
+    $("#talismans_list").highlight($(this).val());
+  });
+  $("#weapons_search").keyup(function() {
+    $("#weapons_list").unhighlight();
+    $("#weapons_list").highlight($(this).val());
+  });
+  $("#whetstones_search").keyup(function() {
+    $("#whetstones_list").unhighlight();
+    $("#whetstones_list").highlight($(this).val());
   });
 });
 })( jQuery );
