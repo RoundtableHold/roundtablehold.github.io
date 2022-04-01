3c3b6905e927c4f9d5c8726da321d0285f8b1e84
dannygils <danielgilfeather@gmail.com>
2022-04-01 16:14:28 -0400

    talisman drops linked to bosses

--- js/item_links.js
+++ js/item_links.js
@@ -94,5 +94,175 @@
       $("#npc_quests_21_5").prop("checked", checked);
       window.onCheckbox("#npc_quests_21_5");
     });
+    $("#bosses_2_5").click(function () {
+      var checked = $(this).prop("checked");
+      $("#talismans_2_2").prop("checked", checked);
+      window.onCheckbox("#talismans_2_2");
+    });
+    $("#talismans_2_2").click(function () {
+      var checked = $(this).prop("checked");
+      $("#bosses_2_5").prop("checked", checked);
+      window.onCheckbox("#bosses_2_5");
+    });
+    $("#bosses_2_4").click(function () {
+      var checked = $(this).prop("checked");
+      $("#talismans_2_3").prop("checked", checked);
+      window.onCheckbox("#talismans_2_3");
+    });
+    $("#talismans_2_3").click(function () {
+      var checked = $(this).prop("checked");
+      $("#bosses_2_4").prop("checked", checked);
+      window.onCheckbox("#bosses_2_4");
+    });
+    $("#bosses_2_9").click(function () {
+      var checked = $(this).prop("checked");
+      $("#talismans_2_13").prop("checked", checked);
+      window.onCheckbox("#talismans_2_13");
+    });
+    $("#talismans_2_13").click(function () {
+      var checked = $(this).prop("checked");
+      $("#bosses_2_9").prop("checked", checked);
+      window.onCheckbox("#bosses_2_9");
+    });
+    $("#bosses_2_21").click(function () {
+      var checked = $(this).prop("checked");
+      $("#talismans_2_17").prop("checked", checked);
+      window.onCheckbox("#talismans_2_17");
+    });
+    $("#talismans_2_17").click(function () {
+      var checked = $(this).prop("checked");
+      $("#bosses_2_21").prop("checked", checked);
+      window.onCheckbox("#bosses_2_21");
+    });
+    $("#bosses_2_10").click(function () {
+      var checked = $(this).prop("checked");
+      $("#talismans_2_18").prop("checked", checked);
+      window.onCheckbox("#talismans_2_18");
+    });
+    $("#talismans_2_18").click(function () {
+      var checked = $(this).prop("checked");
+      $("#bosses_2_10").prop("checked", checked);
+      window.onCheckbox("#bosses_2_10");
+    });
+    $("#bosses_1_10").click(function () {
+      var checked = $(this).prop("checked");
+      $("#talismans_1_6").prop("checked", checked);
+      window.onCheckbox("#talismans_1_6");
+    });
+    $("#talismans_1_6").click(function () {
+      var checked = $(this).prop("checked");
+      $("#bosses_1_10").prop("checked", checked);
+      window.onCheckbox("#bosses_1_10");
+    });
+    $("#bosses_4_17").click(function () {
+      var checked = $(this).prop("checked");
+      $("#talismans_4_1").prop("checked", checked);
+      window.onCheckbox("#talismans_4_1");
+    });
+    $("#talismans_4_1").click(function () {
+      var checked = $(this).prop("checked");
+      $("#bosses_4_17").prop("checked", checked);
+      window.onCheckbox("#bosses_4_17");
+    });
+    $("#bosses_4_37").click(function () {
+      var checked = $(this).prop("checked");
+      $("#talismans_4_6").prop("checked", checked);
+      window.onCheckbox("#talismans_4_6");
+    });
+    $("#bosses_4_1").click(function () {
+      var checked = $(this).prop("checked");
+      $("#talismans_4_10").prop("checked", checked);
+      window.onCheckbox("#talismans_4_10");
+    });
+    $("#bosses_4_8").click(function () {
+      var checked = $(this).prop("checked");
+      $("#talismans_4_13").prop("checked", checked);
+      window.onCheckbox("#talismans_4_13");
+    });
+    $("#bosses_4_26").click(function () {
+      var checked = $(this).prop("checked");
+      $("#talismans_4_17").prop("checked", checked);
+      window.onCheckbox("#talismans_4_17");
+    });
+    $("#talismans_4_6").click(function () {
+      var checked = $(this).prop("checked");
+      $("#bosses_4_37").prop("checked", checked);
+      window.onCheckbox("#bosses_4_37");
+    });
+    $("#talismans_4_10").click(function () {
+      var checked = $(this).prop("checked");
+      $("#bosses_4_1").prop("checked", checked);
+      window.onCheckbox("#bosses_4_1");
+    });
+    $("#talismans_4_13").click(function () {
+      var checked = $(this).prop("checked");
+      $("#bosses_4_8").prop("checked", checked);
+      window.onCheckbox("#bosses_4_8");
+    });
+    $("#talismans_4_17").click(function () {
+      var checked = $(this).prop("checked");
+      $("#bosses_4_26").prop("checked", checked);
+      window.onCheckbox("#bosses_4_26");
+    });
+    $("#bosses_5_14").click(function () {
+      var checked = $(this).prop("checked");
+      $("#talismans_5_2").prop("checked", checked);
+      window.onCheckbox("#talismans_5_2");
+    });
+    $("#bosses_5_21").click(function () {
+      var checked = $(this).prop("checked");
+      $("#talismans_5_7").prop("checked", checked);
+      window.onCheckbox("#talismans_5_7");
+    });
+    $("#bosses_6_1").click(function () {
+      var checked = $(this).prop("checked");
+      $("#talismans_6_5").prop("checked", checked);
+      window.onCheckbox("#talismans_6_5");
+    });
+    $("#bosses_7_4").click(function () {
+      var checked = $(this).prop("checked");
+      $("#talismans_6_10").prop("checked", checked);
+      window.onCheckbox("#talismans_6_10");
+    });
+    $("#bosses_7_13").click(function () {
+      var checked = $(this).prop("checked");
+      $("#talismans_7_5").prop("checked", checked);
+      window.onCheckbox("#talismans_7_5");
+    });
+    $("#bosses_8_3").click(function () {
+      var checked = $(this).prop("checked");
+      $("#talismans_8_1").prop("checked", checked);
+      window.onCheckbox("#talismans_8_1");
+    });
+    $("#talismans_5_2").click(function () {
+      var checked = $(this).prop("checked");
+      $("#bosses_5_14").prop("checked", checked);
+      window.onCheckbox("#bosses_5_14");
+    });
+    $("#talismans_5_7").click(function () {
+      var checked = $(this).prop("checked");
+      $("#bosses_5_21").prop("checked", checked);
+      window.onCheckbox("#bosses_5_21");
+    });
+    $("#talismans_6_5").click(function () {
+      var checked = $(this).prop("checked");
+      $("#bosses_6_1").prop("checked", checked);
+      window.onCheckbox("#bosses_6_1");
+    });
+    $("#talismans_6_10").click(function () {
+      var checked = $(this).prop("checked");
+      $("#bosses_7_4").prop("checked", checked);
+      window.onCheckbox("#bosses_7_4");
+    });
+    $("#talismans_7_5").click(function () {
+      var checked = $(this).prop("checked");
+      $("#bosses_7_13").prop("checked", checked);
+      window.onCheckbox("#bosses_7_13");
+    });
+    $("#talismans_8_1").click(function () {
+      var checked = $(this).prop("checked");
+      $("#bosses_8_3").prop("checked", checked);
+      window.onCheckbox("#bosses_8_3");
+    });
   });
 })( jQuery );
