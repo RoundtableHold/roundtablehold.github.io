c24651e876bdbce1ff27e3768a26ed707a220f62
Thomas Graham <thomas.graham18@outlook.com>
2022-04-05 02:29:28 -0300

    requested changes completed

--- js/item_links.js
+++ js/item_links.js
@@ -16,75 +16,140 @@
       $("#weapons_28_9").prop("checked", checked);
       $("#weapons_28_9").each(function(idx, el) {window.onCheckbox(el)});
     });
-    $("#remembrances_mausoleums_1_2").click(function () {
+    $("#remembrances_mausoleums_1_2_1").click(function () {
       var checked = $(this).prop("checked");
-      $("#sorceries_6_5,#weapons_29_10").prop("checked", checked);
-      $("#sorceries_6_5,#weapons_29_10").each(function(idx, el) {window.onCheckbox(el)});
+      $("#sorceries_6_5").prop("checked", checked);
+      $("#sorceries_6_5").each(function(idx, el) {window.onCheckbox(el)});
     });
-    $("#remembrances_mausoleums_1_3").click(function () {
+    $("#remembrances_mausoleums_1_2_2").click(function () {
       var checked = $(this).prop("checked");
-      $("#weapons_5_6,#weapons_12_4").prop("checked", checked);
-      $("#weapons_5_6,#weapons_12_4").each(function(idx, el) {window.onCheckbox(el)});
+      $("#weapons_29_10").prop("checked", checked);
+      $("#weapons_29_10").each(function(idx, el) {window.onCheckbox(el)});
     });
-    $("#remembrances_mausoleums_1_4").click(function () {
+    $("#remembrances_mausoleums_1_3_1").click(function () {
       var checked = $(this).prop("checked");
-      $("#weapons_19_10,#talismans_3_4").prop("checked", checked);
-      $("#weapons_19_10,#talismans_3_4").each(function(idx, el) {window.onCheckbox(el)});
+      $("#weapons_5_6").prop("checked", checked);
+      $("#weapons_5_6").each(function(idx, el) {window.onCheckbox(el)});
     });
-    $("#remembrances_mausoleums_1_5").click(function () {
+    $("#remembrances_mausoleums_1_3_2").click(function () {
       var checked = $(this).prop("checked");
-      $("#ashesofwar_8_10,#weapons_21_5").prop("checked", checked);
-      $("#ashesofwar_8_10,#weapons_21_5").each(function(idx, el) {window.onCheckbox(el)});
+      $("#weapons_12_4").prop("checked", checked);
+      $("#weapons_12_4").each(function(idx, el) {window.onCheckbox(el)});
     });
-    $("#remembrances_mausoleums_1_6").click(function () {
+    $("#remembrances_mausoleums_1_4_1").click(function () {
       var checked = $(this).prop("checked");
-      $("#weapons_6_6,#ashesofwar_5_7").prop("checked", checked);
-      $("#weapons_6_6,#ashesofwar_5_7").each(function(idx, el) {window.onCheckbox(el)});
+      $("#weapons_19_10").prop("checked", checked);
+      $("#weapons_19_10").each(function(idx, el) {window.onCheckbox(el)});
     });
-    $("#remembrances_mausoleums_1_7").click(function () {
+    $("#remembrances_mausoleums_1_4_2").click(function () {
       var checked = $(this).prop("checked");
-      $("#weapons_5_3,#incantations_5_5").prop("checked", checked);
-      $("#weapons_5_3,#incantations_5_5").each(function(idx, el) {window.onCheckbox(el)});
+      $("#talismans_3_4").prop("checked", checked);
+      $("#talismans_3_4").each(function(idx, el) {window.onCheckbox(el)});
     });
-    $("#remembrances_mausoleums_1_8").click(function () {
+    $("#remembrances_mausoleums_1_5_1").click(function () {
       var checked = $(this).prop("checked");
-      $("#sorceries_9_6,#weapons_14_18").prop("checked", checked);
-      $("#sorceries_9_6,#weapons_14_18").each(function(idx, el) {window.onCheckbox(el)});
+      $("#ashesofwar_8_10").prop("checked", checked);
+      $("#ashesofwar_8_10").each(function(idx, el) {window.onCheckbox(el)});
     });
-    $("#remembrances_mausoleums_1_9").click(function () {
+    $("#remembrances_mausoleums_1_5_2").click(function () {
       var checked = $(this).prop("checked");
-      $("#weapons_27_6,#incantations_6_1").prop("checked", checked);
-      $("#weapons_27_6,#incantations_6_1").each(function(idx, el) {window.onCheckbox(el)});
+      $("#weapons_21_5").prop("checked", checked);
+      $("#weapons_21_5").each(function(idx, el) {window.onCheckbox(el)});
     });
-    $("#remembrances_mausoleums_1_10").click(function () {
+    $("#remembrances_mausoleums_1_6_1").click(function () {
+      var checked = $(this).prop("checked");
+      $("#weapons_6_6").prop("checked", checked);
+      $("#weapons_6_6").each(function(idx, el) {window.onCheckbox(el)});
+    });
+    $("#remembrances_mausoleums_1_6_2").click(function () {
+      var checked = $(this).prop("checked");
+      $("#ashesofwar_5_7").prop("checked", checked);
+      $("#ashesofwar_5_7").each(function(idx, el) {window.onCheckbox(el)});
+    });
+    $("#remembrances_mausoleums_1_7_1").click(function () {
+      var checked = $(this).prop("checked");
+      $("#weapons_5_3").prop("checked", checked);
+      $("#weapons_5_3").each(function(idx, el) {window.onCheckbox(el)});
+    });
+    $("#remembrances_mausoleums_1_7_2").click(function () {
+      var checked = $(this).prop("checked");
+      $("#incantations_5_5").prop("checked", checked);
+      $("#incantations_5_5").each(function(idx, el) {window.onCheckbox(el)});
+    });
+    $("#remembrances_mausoleums_1_8_1").click(function () {
+      var checked = $(this).prop("checked");
+      $("#sorceries_9_6").prop("checked", checked);
+      $("#sorceries_9_6").each(function(idx, el) {window.onCheckbox(el)});
+    });
+    $("#remembrances_mausoleums_1_8_2").click(function () {
+      var checked = $(this).prop("checked");
+      $("#weapons_14_18").prop("checked", checked);
+      $("#weapons_14_18").each(function(idx, el) {window.onCheckbox(el)});
+    });
+    $("#remembrances_mausoleums_1_9_1").click(function () {
+      var checked = $(this).prop("checked");
+      $("#weapons_27_6").prop("checked", checked);
+      $("#weapons_27_6").each(function(idx, el) {window.onCheckbox(el)});
+    });
+    $("#remembrances_mausoleums_1_9_2").click(function () {
+      var checked = $(this).prop("checked");
+      $("#incantations_6_1").prop("checked", checked);
+      $("#incantations_6_1").each(function(idx, el) {window.onCheckbox(el)});
+    });
+    $("#remembrances_mausoleums_1_10_1").click(function () {
       var checked = $(this).prop("checked");
       $("#weapons_8_9").prop("checked", checked);
       $("#weapons_8_9").each(function(idx, el) {window.onCheckbox(el)});
     });
-    $("#remembrances_mausoleums_1_11").click(function () {
+    $("#remembrances_mausoleums_1_11_1").click(function () {
       var checked = $(this).prop("checked");
-      $("#weapons_16_4,#incantations_3_11").prop("checked", checked);
-      $("#weapons_16_4,#incantations_3_11").each(function(idx, el) {window.onCheckbox(el)});
+      $("#weapons_16_4").prop("checked", checked);
+      $("#weapons_16_4").each(function(idx, el) {window.onCheckbox(el)});
     });
-    $("#remembrances_mausoleums_1_12").click(function () {
+    $("#remembrances_mausoleums_1_11_2").click(function () {
       var checked = $(this).prop("checked");
-      $("#incantations_4_6,#incantations_4_3").prop("checked", checked);
-      $("#incantations_4_6,#incantations_4_3").each(function(idx, el) {window.onCheckbox(el)});
+      $("#incantations_3_11").prop("checked", checked);
+      $("#incantations_3_11").each(function(idx, el) {window.onCheckbox(el)});
     });
-    $("#remembrances_mausoleums_1_13").click(function () {
+    $("#remembrances_mausoleums_1_12_1").click(function () {
       var checked = $(this).prop("checked");
-      $("#weapons_20_15,#weapons_14_21").prop("checked", checked);
-      $("#weapons_20_15,#weapons_14_21").each(function(idx, el) {window.onCheckbox(el)});
+      $("#incantations_4_6").prop("checked", checked);
+      $("#incantations_4_6").each(function(idx, el) {window.onCheckbox(el)});
     });
-    $("#remembrances_mausoleums_1_14").click(function () {
+    $("#remembrances_mausoleums_1_12_2").click(function () {
       var checked = $(this).prop("checked");
-      $("#weapons_17_8,#incantations_11_4").prop("checked", checked);
-      $("#weapons_17_8,#incantations_11_4").each(function(idx, el) {window.onCheckbox(el)});
+      $("#incantations_4_3").prop("checked", checked);
+      $("#incantations_4_3").each(function(idx, el) {window.onCheckbox(el)});
     });
-    $("#remembrances_mausoleums_1_15").click(function () {
+    $("#remembrances_mausoleums_1_13_1").click(function () {
       var checked = $(this).prop("checked");
-      $("#weapons_24_6,#incantations_2_1").prop("checked", checked);
-      $("#weapons_24_6,#incantations_2_1").each(function(idx, el) {window.onCheckbox(el)});
+      $("#weapons_20_15").prop("checked", checked);
+      $("#weapons_20_15").each(function(idx, el) {window.onCheckbox(el)});
+    });
+    $("#remembrances_mausoleums_1_13_2").click(function () {
+      var checked = $(this).prop("checked");
+      $("#weapons_14_21]").prop("checked", checked);
+      $("#weapons_14_21]").each(function(idx, el) {window.onCheckbox(el)});
+    });
+    $("#remembrances_mausoleums_1_14_1").click(function () {
+      var checked = $(this).prop("checked");
+      $("#weapons_17_8").prop("checked", checked);
+      $("#weapons_17_8").each(function(idx, el) {window.onCheckbox(el)});
+    });
+    $("#remembrances_mausoleums_1_14_2").click(function () {
+      var checked = $(this).prop("checked");
+      $("#incantations_11_4").prop("checked", checked);
+      $("#incantations_11_4").each(function(idx, el) {window.onCheckbox(el)});
+    });
+    $("#remembrances_mausoleums_1_15_1").click(function () {
+      var checked = $(this).prop("checked");
+      $("#weapons_24_6").prop("checked", checked);
+      $("#weapons_24_6").each(function(idx, el) {window.onCheckbox(el)});
+    });
+    $("#remembrances_mausoleums_1_15_2").click(function () {
+      var checked = $(this).prop("checked");
+      $("#incantations_2_1").prop("checked", checked);
+      $("#incantations_2_1").each(function(idx, el) {window.onCheckbox(el)});
     });
   });
 })( jQuery );
