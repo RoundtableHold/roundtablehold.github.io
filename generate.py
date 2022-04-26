93b1369665145e5fbb2a336f3760e5589a7a284e
Ben Lambeth <accounts@lambethben.com>
2022-04-26 10:36:42 -0400

    Add android badge

--- generate.py
+++ generate.py
@@ -265,7 +265,13 @@ def make_index():
                             with div(cls='col'):
                                 with div(cls='card shadow h-100'):
                                     with div(cls='card-body'):
-                                        raw('<a href="https://apps.apple.com/us/app/elden-ring-guides/id1620436088?itsct=apps_box_badge&amp;itscg=30200" style="display: inline-block; overflow: hidden; border-radius: 13px; width: 180px; height: 60px;"><img src="https://tools.applemediaservices.com/api/badges/download-on-the-app-store/black/en-us?size=250x83&amp;releaseDate=1650585600&h=3eb10370b9c49cf5b5dde5ca0352f23a" alt="Download on the App Store" style="border-radius: 13px; width: 180px; height: 60px;"></a>')
+                                        h5('Get the Apps!', cls='card-title text-center')
+                                        with div(style='width: 180px;', cls='badge'):
+                                            with a(href='https://apps.apple.com/us/app/elden-ring-guides/id1620436088?itsct=apps_box_badge&amp;itscg=30200'):
+                                                img(src="https://tools.applemediaservices.com/api/badges/download-on-the-app-store/black/en-us?size=250x83&amp;releaseDate=1650585600&h=3eb10370b9c49cf5b5dde5ca0352f23a", alt="Download on the App Store", style='margin: 6%; width: 88%;')
+                                        with div(style='width: 180px;', cls='badge'):
+                                            with a(href='https://play.google.com/store/apps/details?id=com.roundtablehold.eldenringguides&pcampaignid=pcampaignidMKT-Other-global-all-co-prtnr-py-PartBadge-Mar2515-1', style='width: 100%;'):
+                                                img(src='https://play.google.com/intl/en_us/badges/static/images/badges/en_badge_web_generic.png', style='width: 100%;')
                     with div(cls="col-md-4 col-12"):
                         with div(cls='card shadow'):
                             with div(cls="card-body uncolor-links"):
