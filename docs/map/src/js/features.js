92251daac5614d432551d3c679b003f9a38ddc5d
Olivia <livwebspam@gmail.com>
2022-04-30 15:53:17 -0600

    Update graces.yaml
    Map locations for Caelid graces

--- docs/map/src/js/features.js
+++ docs/map/src/js/features.js
@@ -1149,13 +1149,710 @@ const feature_data = [
           "title": "Godrick the Grafted"
         },
         "type": "Feature"
+      },
+      {
+        "geometry": {
+          "coordinates": [
+            4658,
+            6243
+          ],
+          "type": "Point"
+        },
+        "id": "graces_193",
+        "properties": {
+          "icon": "/map/icons/MENU_MAP_01_Bonfire.png",
+          "id": "graces_193",
+          "link": "/checklists/sitesof_grace.html#item_193",
+          "title": "Smoldering Church"
+        },
+        "type": "Feature"
+      },
+      {
+        "geometry": {
+          "coordinates": [
+            4842,
+            6339
+          ],
+          "type": "Point"
+        },
+        "id": "graces_194",
+        "properties": {
+          "icon": "/map/icons/MENU_MAP_01_Bonfire.png",
+          "id": "graces_194",
+          "link": "/checklists/sitesof_grace.html#item_194",
+          "title": "Rotview Balcony"
+        },
+        "type": "Feature"
+      },
+      {
+        "geometry": {
+          "coordinates": [
+            5094,
+            6292
+          ],
+          "type": "Point"
+        },
+        "id": "graces_195",
+        "properties": {
+          "icon": "/map/icons/MENU_MAP_01_Bonfire.png",
+          "id": "graces_195",
+          "link": "/checklists/sitesof_grace.html#item_195",
+          "title": "Caelem Ruins"
+        },
+        "type": "Feature"
+      },
+      {
+        "geometry": {
+          "coordinates": [
+            4859,
+            6610
+          ],
+          "type": "Point"
+        },
+        "id": "graces_196",
+        "properties": {
+          "icon": "/map/icons/MENU_MAP_01_Bonfire.png",
+          "id": "graces_196",
+          "link": "/checklists/sitesof_grace.html#item_196",
+          "title": "Fort Gael North"
+        },
+        "type": "Feature"
+      },
+      {
+        "geometry": {
+          "coordinates": [
+            5159,
+            6524
+          ],
+          "type": "Point"
+        },
+        "id": "graces_197",
+        "properties": {
+          "icon": "/map/icons/MENU_MAP_01_Bonfire.png",
+          "id": "graces_197",
+          "link": "/checklists/sitesof_grace.html#item_197",
+          "title": "Smoldering Wall"
+        },
+        "type": "Feature"
+      },
+      {
+        "geometry": {
+          "coordinates": [
+            5126,
+            6997
+          ],
+          "type": "Point"
+        },
+        "id": "graces_198",
+        "properties": {
+          "icon": "/map/icons/MENU_MAP_01_Bonfire.png",
+          "id": "graces_198",
+          "link": "/checklists/sitesof_grace.html#item_198",
+          "title": "Caelid Highway South"
+        },
+        "type": "Feature"
+      },
+      {
+        "geometry": {
+          "coordinates": [
+            5286,
+            7194
+          ],
+          "type": "Point"
+        },
+        "id": "graces_199",
+        "properties": {
+          "icon": "/map/icons/MENU_MAP_01_Bonfire.png",
+          "id": "graces_199",
+          "link": "/checklists/sitesof_grace.html#item_199",
+          "title": "Cathedral of Dragon Communion"
+        },
+        "type": "Feature"
+      },
+      {
+        "geometry": {
+          "coordinates": [
+            5507,
+            6975
+          ],
+          "type": "Point"
+        },
+        "id": "graces_200",
+        "properties": {
+          "icon": "/map/icons/MENU_MAP_01_Bonfire.png",
+          "id": "graces_200",
+          "link": "/checklists/sitesof_grace.html#item_200",
+          "title": "Southern Aeonia Swamp Bank"
+        },
+        "type": "Feature"
+      },
+      {
+        "geometry": {
+          "coordinates": [
+            5631,
+            6471
+          ],
+          "type": "Point"
+        },
+        "id": "graces_201",
+        "properties": {
+          "icon": "/map/icons/MENU_MAP_01_Bonfire.png",
+          "id": "graces_201",
+          "link": "/checklists/sitesof_grace.html#item_201",
+          "title": "Sellia Backstreets"
+        },
+        "type": "Feature"
+      },
+      {
+        "geometry": {
+          "coordinates": [
+            5600,
+            6559
+          ],
+          "type": "Point"
+        },
+        "id": "graces_202",
+        "properties": {
+          "icon": "/map/icons/MENU_MAP_01_Bonfire.png",
+          "id": "graces_202",
+          "link": "/checklists/sitesof_grace.html#item_202",
+          "title": "Sellia Under-Stair"
+        },
+        "type": "Feature"
+      },
+      {
+        "geometry": {
+          "coordinates": [
+            5601,
+            6452
+          ],
+          "type": "Point"
+        },
+        "id": "graces_203",
+        "properties": {
+          "icon": "/map/icons/MENU_MAP_01_Bonfire.png",
+          "id": "graces_203",
+          "link": "/checklists/sitesof_grace.html#item_203",
+          "title": "Chair-Crypt of Sellia"
+        },
+        "type": "Feature"
+      },
+      {
+        "geometry": {
+          "coordinates": [
+            5771,
+            6646
+          ],
+          "type": "Point"
+        },
+        "id": "graces_204",
+        "properties": {
+          "icon": "/map/icons/MENU_MAP_01_Bonfire.png",
+          "id": "graces_204",
+          "link": "/checklists/sitesof_grace.html#item_204",
+          "title": "Church of the Plague"
+        },
+        "type": "Feature"
+      },
+      {
+        "geometry": {
+          "coordinates": [
+            5356,
+            6271
+          ],
+          "type": "Point"
+        },
+        "id": "graces_205",
+        "properties": {
+          "icon": "/map/icons/MENU_MAP_01_Bonfire.png",
+          "id": "graces_205",
+          "link": "/checklists/sitesof_grace.html#item_205",
+          "title": "Deep Siofra Well"
+        },
+        "type": "Feature"
+      },
+      {
+        "geometry": {
+          "coordinates": [
+            5693,
+            7378
+          ],
+          "type": "Point"
+        },
+        "id": "graces_206",
+        "properties": {
+          "icon": "/map/icons/MENU_MAP_01_Bonfire.png",
+          "id": "graces_206",
+          "link": "/checklists/sitesof_grace.html#item_206",
+          "title": "Impassable Greatbridge"
+        },
+        "type": "Feature"
+      },
+      {
+        "geometry": {
+          "coordinates": [
+            6110,
+            7341
+          ],
+          "type": "Point"
+        },
+        "id": "graces_207",
+        "properties": {
+          "icon": "/map/icons/MENU_MAP_01_Bonfire.png",
+          "id": "graces_207",
+          "link": "/checklists/sitesof_grace.html#item_207",
+          "title": "Chamber Outside the Plaza"
+        },
+        "type": "Feature"
+      },
+      {
+        "geometry": {
+          "coordinates": [
+            6096,
+            7276
+          ],
+          "type": "Point"
+        },
+        "id": "graces_208",
+        "properties": {
+          "icon": "/map/icons/MENU_MAP_01_Bonfire.png",
+          "id": "graces_208",
+          "link": "/checklists/sitesof_grace.html#item_208",
+          "title": "Redmane Castle Plaza"
+        },
+        "type": "Feature"
+      },
+      {
+        "geometry": {
+          "coordinates": [
+            6226,
+            6824
+          ],
+          "type": "Point"
+        },
+        "id": "graces_209",
+        "properties": {
+          "icon": "/map/icons/MENU_MAP_01_Bonfire.png",
+          "id": "graces_209",
+          "link": "/checklists/sitesof_grace.html#item_209",
+          "title": "Starscourge Radahn"
+        },
+        "type": "Feature"
+      },
+      {
+        "geometry": {
+          "coordinates": [
+            4932,
+            6205
+          ],
+          "type": "Point"
+        },
+        "id": "graces_210",
+        "properties": {
+          "icon": "/map/icons/MENU_MAP_01_Bonfire.png",
+          "id": "graces_210",
+          "link": "/checklists/sitesof_grace.html#item_210",
+          "title": ""
+        },
+        "type": "Feature"
+      },
+      {
+        "geometry": {
+          "coordinates": [
+            5338,
+            7225
+          ],
+          "type": "Point"
+        },
+        "id": "graces_211",
+        "properties": {
+          "icon": "/map/icons/MENU_MAP_01_Bonfire.png",
+          "id": "graces_211",
+          "link": "/checklists/sitesof_grace.html#item_211",
+          "title": "Caelid Catacombs"
+        },
+        "type": "Feature"
+      },
+      {
+        "geometry": {
+          "coordinates": [
+            6310,
+            6148
+          ],
+          "type": "Point"
+        },
+        "id": "graces_212",
+        "properties": {
+          "icon": "/map/icons/MENU_MAP_01_Bonfire.png",
+          "id": "graces_212",
+          "link": "/checklists/sitesof_grace.html#item_212",
+          "title": "War-Dead Catacombs"
+        },
+        "type": "Feature"
+      },
+      {
+        "geometry": {
+          "coordinates": [
+            5388,
+            6480
+          ],
+          "type": "Point"
+        },
+        "id": "graces_213",
+        "properties": {
+          "icon": "/map/icons/MENU_MAP_01_Bonfire.png",
+          "id": "graces_213",
+          "link": "/checklists/sitesof_grace.html#item_213",
+          "title": "Abandoned Cave"
+        },
+        "type": "Feature"
+      },
+      {
+        "geometry": {
+          "coordinates": [
+            4926,
+            6747
+          ],
+          "type": "Point"
+        },
+        "id": "graces_214",
+        "properties": {
+          "icon": "/map/icons/MENU_MAP_01_Bonfire.png",
+          "id": "graces_214",
+          "link": "/checklists/sitesof_grace.html#item_214",
+          "title": "Gaol Cave"
+        },
+        "type": "Feature"
+      },
+      {
+        "geometry": {
+          "coordinates": [
+            4794,
+            6490
+          ],
+          "type": "Point"
+        },
+        "id": "graces_215",
+        "properties": {
+          "icon": "/map/icons/MENU_MAP_01_Bonfire.png",
+          "id": "graces_215",
+          "link": "/checklists/sitesof_grace.html#item_215",
+          "title": "Gael Tunnel"
+        },
+        "type": "Feature"
+      },
+      {
+        "geometry": {
+          "coordinates": [
+            4700,
+            6415
+          ],
+          "type": "Point"
+        },
+        "id": "graces_216",
+        "properties": {
+          "icon": "/map/icons/MENU_MAP_01_Bonfire.png",
+          "id": "graces_216",
+          "link": "/checklists/sitesof_grace.html#item_216",
+          "title": "Rear Gael Tunnel Entrance"
+        },
+        "type": "Feature"
+      },
+      {
+        "geometry": {
+          "coordinates": [
+            5520,
+            6416
+          ],
+          "type": "Point"
+        },
+        "id": "graces_217",
+        "properties": {
+          "icon": "/map/icons/MENU_MAP_01_Bonfire.png",
+          "id": "graces_217",
+          "link": "/checklists/sitesof_grace.html#item_217",
+          "title": "Sellia Crystal Tunnel"
+        },
+        "type": "Feature"
+      },
+      {
+        "geometry": {
+          "coordinates": [
+            5245,
+            6816
+          ],
+          "type": "Point"
+        },
+        "id": "graces_218",
+        "properties": {
+          "icon": "/map/icons/MENU_MAP_01_Bonfire.png",
+          "id": "graces_218",
+          "link": "/checklists/sitesof_grace.html#item_218",
+          "title": "Aeonia Swamp Shore"
+        },
+        "type": "Feature"
+      },
+      {
+        "geometry": {
+          "coordinates": [
+            5140,
+            6764
+          ],
+          "type": "Point"
+        },
+        "id": "graces_219",
+        "properties": {
+          "icon": "/map/icons/MENU_MAP_01_Bonfire.png",
+          "id": "graces_219",
+          "link": "/checklists/sitesof_grace.html#item_219",
+          "title": "Astray from Caelid Highway North"
+        },
+        "type": "Feature"
+      },
+      {
+        "geometry": {
+          "coordinates": [
+            5481,
+            6734
+          ],
+          "type": "Point"
+        },
+        "id": "graces_220",
+        "properties": {
+          "icon": "/map/icons/MENU_MAP_01_Bonfire.png",
+          "id": "graces_220",
+          "link": "/checklists/sitesof_grace.html#item_220",
+          "title": "Heart of Aeonia"
+        },
+        "type": "Feature"
+      },
+      {
+        "geometry": {
+          "coordinates": [
+            5443,
+            6666
+          ],
+          "type": "Point"
+        },
+        "id": "graces_221",
+        "properties": {
+          "icon": "/map/icons/MENU_MAP_01_Bonfire.png",
+          "id": "graces_221",
+          "link": "/checklists/sitesof_grace.html#item_221",
+          "title": "Inner Aeonia"
+        },
+        "type": "Feature"
+      },
+      {
+        "geometry": {
+          "coordinates": [
+            5356,
+            6205
+          ],
+          "type": "Point"
+        },
+        "id": "graces_222",
+        "properties": {
+          "icon": "/map/icons/MENU_MAP_01_Bonfire.png",
+          "id": "graces_222",
+          "link": "/checklists/sitesof_grace.html#item_222",
+          "title": "Dragonbarrow West"
+        },
+        "type": "Feature"
+      },
+      {
+        "geometry": {
+          "coordinates": [
+            5159,
+            6105
+          ],
+          "type": "Point"
+        },
+        "id": "graces_223",
+        "properties": {
+          "icon": "/map/icons/MENU_MAP_01_Bonfire.png",
+          "id": "graces_223",
+          "link": "/checklists/sitesof_grace.html#item_223",
+          "title": "Isolated Merchant's Shack"
+        },
+        "type": "Feature"
+      },
+      {
+        "geometry": {
+          "coordinates": [
+            5938,
+            6406
+          ],
+          "type": "Point"
+        },
+        "id": "graces_224",
+        "properties": {
+          "icon": "/map/icons/MENU_MAP_01_Bonfire.png",
+          "id": "graces_224",
+          "link": "/checklists/sitesof_grace.html#item_224",
+          "title": "Fort Faroth"
+        },
+        "type": "Feature"
+      },
+      {
+        "geometry": {
+          "coordinates": [
+            5838,
+            6152
+          ],
+          "type": "Point"
+        },
+        "id": "graces_225",
+        "properties": {
+          "icon": "/map/icons/MENU_MAP_01_Bonfire.png",
+          "id": "graces_225",
+          "link": "/checklists/sitesof_grace.html#item_225",
+          "title": "Dragonbarrow Fork"
+        },
+        "type": "Feature"
+      },
+      {
+        "geometry": {
+          "coordinates": [
+            6291,
+            6006
+          ],
+          "type": "Point"
+        },
+        "id": "graces_226",
+        "properties": {
+          "icon": "/map/icons/MENU_MAP_01_Bonfire.png",
+          "id": "graces_226",
+          "link": "/checklists/sitesof_grace.html#item_226",
+          "title": "Lenne's Rise"
+        },
+        "type": "Feature"
+      },
+      {
+        "geometry": {
+          "coordinates": [
+            6166,
+            5732
+          ],
+          "type": "Point"
+        },
+        "id": "graces_227",
+        "properties": {
+          "icon": "/map/icons/MENU_MAP_01_Bonfire.png",
+          "id": "graces_227",
+          "link": "/checklists/sitesof_grace.html#item_227",
+          "title": "Farum Greatbridge"
+        },
+        "type": "Feature"
+      },
+      {
+        "geometry": {
+          "coordinates": [
+            5968,
+            5488
+          ],
+          "type": "Point"
+        },
+        "id": "graces_228",
+        "properties": {
+          "icon": "/map/icons/MENU_MAP_01_Bonfire.png",
+          "id": "graces_228",
+          "link": "/checklists/sitesof_grace.html#item_228",
+          "title": "Bestial Sanctum"
+        },
+        "type": "Feature"
+      },
+      {
+        "geometry": {
+          "coordinates": [
+            5886,
+            6463
+          ],
+          "type": "Point"
+        },
+        "id": "graces_229",
+        "properties": {
+          "icon": "/map/icons/MENU_MAP_01_Bonfire.png",
+          "id": "graces_229",
+          "link": "/checklists/sitesof_grace.html#item_229",
+          "title": "Sellia Hideaway"
+        },
+        "type": "Feature"
+      },
+      {
+        "geometry": {
+          "coordinates": [
+            6017,
+            6187
+          ],
+          "type": "Point"
+        },
+        "id": "graces_230",
+        "properties": {
+          "icon": "/map/icons/MENU_MAP_01_Bonfire.png",
+          "id": "graces_230",
+          "link": "/checklists/sitesof_grace.html#item_230",
+          "title": "Dragonbarrow Cave"
+        },
+        "type": "Feature"
+      },
+      {
+        "geometry": {
+          "coordinates": [
+            5580,
+            6025
+          ],
+          "type": "Point"
+        },
+        "id": "graces_231",
+        "properties": {
+          "icon": "/map/icons/MENU_MAP_01_Bonfire.png",
+          "id": "graces_231",
+          "link": "/checklists/sitesof_grace.html#item_231",
+          "title": "Divine Tower of Caelid: Center"
+        },
+        "type": "Feature"
+      },
+      {
+        "geometry": {
+          "coordinates": [
+            5592,
+            6040
+          ],
+          "type": "Point"
+        },
+        "id": "graces_232",
+        "properties": {
+          "icon": "/map/icons/MENU_MAP_01_Bonfire.png",
+          "id": "graces_232",
+          "link": "/checklists/sitesof_grace.html#item_232",
+          "title": "Divine Tower of Caelid: Basement"
+        },
+        "type": "Feature"
+      },
+      {
+        "geometry": {
+          "coordinates": [
+            5999,
+            4845
+          ],
+          "type": "Point"
+        },
+        "id": "graces_233",
+        "properties": {
+          "icon": "/map/icons/MENU_MAP_01_Bonfire.png",
+          "id": "graces_233",
+          "link": "/checklists/sitesof_grace.html#item_233",
+          "title": "Isolated Divine Tower"
+        },
+        "type": "Feature"
       }
     ],
     "type": "FeatureCollection"
   }
 ];
 const icon_urls = [
-  "/map/icons/MENU_MAP_01_Bonfire.png",
   "/map/icons/MENU_MAP_memo_20.png",
-  "/map/icons/MENU_MAP_09.png"
+  "/map/icons/MENU_MAP_09.png",
+  "/map/icons/MENU_MAP_01_Bonfire.png"
 ]
